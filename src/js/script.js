26f967b2e074a6330cf6dc7d09ff3b43d177a455
Vlad <vladislavkukhar66@gmail.com>
2026-04-07 14:49:04 +0300

    add header and footer links

--- src/js/script.js
+++ src/js/script.js
@@ -34,7 +34,6 @@ try {
             prevEl: ".icon-left-open",
         },
         breakpoints: {
-            // when window width is >= 1200px
             1200: {
                 slidesPerView: 3,
                 spaceBetween: 5,
